78deb14cc981077707ba67132a1e8cadcf7462b1
Alex Nedela <nedelaaa97@yahoo.com>
2024-10-27 22:47:00 +0200

    clase

--- poo1/poo1.cpp
+++ poo1/poo1.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
+#include <vector>
 #include <string>
 
+// Clasa Carte
+class Carte {
+private:
+    std::string titlu;
+    std::string autor;
+    int anPublicare;
+
+public:
+    Carte(const std::string& t, const std::string& a, int an)
+        : titlu(t), autor(a), anPublicare(an) {
+    }
+
+    void afiseazaCarte() const {
+        std::cout << "Titlu: " << titlu << ", Autor: " << autor
+            << ", An Publicare: " << anPublicare << std::endl;
+    }
+};
+
+// Clasa Biblioteca
+class Biblioteca {
+private:
+    std::vector<Carte> carti;
+
+public:
+    void adaugaCarte(const Carte& carte) {
+        carti.push_back(carte);
+    }
+
+    void afiseazaCarti() const {
+        std::cout << "Carti in biblioteca:" << std::endl;
+        for (const auto& carte : carti) {
+            carte.afiseazaCarte();
+        }
+    }
+};
+
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello, " << name << "!" << std::endl;
+    Biblioteca biblioteca;
+
+    Carte carte1("Micul Print", "Antoine de Saint-Exupery", 1943);
+    Carte carte2("1984", "George Orwell", 1949);
+
+    biblioteca.adaugaCarte(carte1);
+    biblioteca.adaugaCarte(carte2);
+
+    biblioteca.afiseazaCarti();
+
     return 0;
 }
+
+
